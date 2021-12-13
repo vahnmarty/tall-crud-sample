8a35eef73a96435601e9a39fd79e68a20380586d
Vahn Marty <vahnmarty@gmail.com>
2021-12-14 00:50:52 +0800

    laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'users'], function(){
+    //Route::get('/', ManageUsers::class);
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
